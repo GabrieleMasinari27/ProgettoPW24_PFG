ffa6542355def915090d0b10734333289521441f
Gabriele-Mazzoleni <g.mazzoleni29@studenti.unibg.it>
2024-05-14 15:51:32 +0200

    modifiche e AUTO1 zeeeh

--- Html_php/index.php
+++ Html_php/index.php
@@ -20,9 +20,20 @@
         include "nav.html";
         ?>
       </div>
+      <div id="nota">
+      <p>NOTA: le operazioni di inserimento, modifica e rimozione dei dati sono disponibili soltanto sulla tabella Targa, come da consegna</p>
+      </div>
     </div>
     <div class="risultato">
-
+      <div>
+        <p id="intro">Benvenuto a PFG Motorizzazione!</p>
+        <p>Questa pagina web permette di visualizzare una collezione di dati relativi a targhe, siano esse ancora attive o gia' restituite; <br></p>
+        <p>Ogni targa ha un collegamento a un veicolo e a un numero variabile revisioni, che possono essere consultate nelle apposite pagine. <br></p>
+        <p>Per iniziare la navigazione selezionare uno dei collegamenti sulla tabella di navigazione (sulla sinistra).</p>
+      </div>
+      <div class="immagini">
+        <img src="./../img/auto1.png" alt="macchinaBianca">
+      </div>
     </div>
 </div>
 </body>
