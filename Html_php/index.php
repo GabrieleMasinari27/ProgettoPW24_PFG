f3521fc99dd4e51b4d79d6bb7a6691959bcfb80c
Gabriele-Mazzoleni <g.mazzoleni29@studenti.unibg.it>
2024-05-14 14:39:55 +0200

    nuova homepage

--- Html_php/index.php
+++ Html_php/index.php
@@ -7,7 +7,7 @@
   <meta charset="utf-8">
   <title>ProgettoPFG_Motorizzazione</title>
 </head>
-<body onload="setVeicolo()">
+<body>
   <?php
   include "header.html";
   include "footer.html";
@@ -20,91 +20,10 @@
         include "nav.html";
         ?>
       </div>
-      <div class="filtro">
-        <form name="form_ricerca" method="post">
-          <input type="number" name="telaio" placeholder="Telaio"><br>
-          <input type="text" name="marca"  placeholder="Marca"><br>
-          <input type="text" name="modello"  placeholder="Modello"><br><br>
-          Data della produzione:
-          <input type="date" name="dataproduzione" placeholder="Data della produzione"><br><br>
-          <label for="scelta">Ordina per:</label>
-          <select id="ordinamento" name="scelta">
-            <option value="ordinamentoNullo">Nessun ordinamento</option>
-            <option value="ordinaNumeroTel">Numero telaio</option>
-            <option value="ordinaMarca">Alfabetico per marca</option>
-            <option value="ordinaModello">Alfabetico perModello</option>
-            <option value="ordinaData">Data Produzione</option>
-          </select>
-          <input type="submit" name="bottonericerca" value="Cerca">
-
-        </form>
-      </div>
     </div>
     <div class="risultato">
-      <?php
-      $numTelaio= "";
-      $marca= "";
-      $modello = "";
-      $dataPro="";
-      $valoreordinamento="";
-      if(count($_POST)>0 ){
-        $numTelaio= $_POST["telaio"];
-        $marca = $_POST["marca"];
-        $modello = $_POST["modello"];
-        $dataPro=$_POST["dataproduzione"];
-        $valoreordinamento=$_POST["scelta"];
-      }
-      $query = queryVeicolo($numTelaio, $marca, $modello, $dataPro, $valoreordinamento);
-      
-      include 'connect.php';
-      try {
-        $result = $conn->query($query);
-      } catch(PDOException$e) {
-        echo "<p>DB Error on Query: " . $e->getMessage() . "</p>";
-        $error = true;
-      }
-      	if(!$error) {
-      ?>
-      <table class="table">
-        <!-- IMPLEMENTARE IL NUMERO DI TARGHE restituite
-        E AGGIUNGERE ID DI TARGA ATTIVA-->
-      <tr class="header">
-        <th>#</th>
-        <th>#Telaio</th>
-        <th>Marca</th>
-        <th>Modello</th>
-        <th>Data di produzione</th>
-        <th>#Targhe Restituite</th>
-        <th>IdTargaAttiva</th>
-      </tr>
-      <?php
-      $i=0;
-      foreach($result as $riga) {
-        $i=$i+1;
-        $numTelaio = $riga["telaio"];
-        $marca = $riga["marca"];
-        $modello = $riga["modello"];
-        $dataPro = $riga["data"];
-        $num_res= $riga["num_restituite"];
-        $targa_att=$riga["targa_attiva"];
-      ?>
-      <tr>
-        <td><?php echo $i; ?></td>
-        <td><?php echo $numTelaio; ?></td>
-        <td><?php echo $marca; ?></td>
-        <td><?php echo $modello; ?></td>
-        <td><?php echo $dataPro; ?></td>
-        <td><?php echo $num_res; ?></td>
-        <td><?php echo $targa_att; ?></td>
-      </tr>
-      <?php
-      }
-      ?>
-    </table>
-      <?php
-        }
-       ?>
-  </div>
+
+    </div>
 </div>
 </body>
 </html>
