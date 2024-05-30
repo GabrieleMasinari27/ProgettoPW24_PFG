2f90bc76398e8af9f677ee0dc0b1776568e69383
GabrieleMasinari27 <g.masinari@studenti.unibg.it>
2024-05-30 11:35:41 +0200

    nulla

--- js/controlli.js
+++ js/controlli.js
@@ -31,10 +31,3 @@ function testoHiddenDiv(testo){
         window.location.href = 'targa.php';
     }, 5000); // reindirizza dopo 2 secondi
 }
-function modificaValoreBottone(bottone) {
-    if (bottone.value === "Cerca") {
-        bottone.value = "Annulla Filtro";
-    } else {
-        bottone.value = "Cerca";
-    }
-}
